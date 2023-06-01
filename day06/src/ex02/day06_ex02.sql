35d6e2e29f285d40743b4c1c381232438012807a
Nyarlathotep Greyfeather <nyarlath@ge-d4.kzn.21-school.ru>
2023-06-01 12:57:35 +0300

    update day 06

--- day06/src/ex02/day06_ex02.sql
+++ day06/src/ex02/day06_ex02.sql
@@ -1,9 +1,7 @@
 SELECT person.name AS person_name,
     menu.pizza_name,
     menu.price,
-    round(
-        menu.price * (1 - person_discounts.discount / 100)
-    ) AS discount_price,
+    (menu.price / 100 * (100 - person_discounts.discount)) AS discount_price,
     pizzeria.name as pizzeria_name
 FROM person_order
     JOIN menu ON person_order.menu_id = menu.id
